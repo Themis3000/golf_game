41e77156129d99486408f29164f1489dd59b90ef
Themi Megas <Tcm4760@gmail.com>
2023-09-26 02:09:34 -0500

    completed serializer

--- core_scripts/level_serializer.gd
+++ core_scripts/level_serializer.gd
@@ -1,5 +1,35 @@
 extends Node
 
-func serialize_level(file_name, scene):
-	print(file_name)
-	print(scene)
+func serialize_level(file_name: String, scene: Node2D):
+	var data = {}
+	var path = "user://" + file_name
+	
+	var ball = scene.get_node("Ball")
+	var ball_position = ball.get_position()
+	var ball_data = {"x": ball_position.x, "y": ball_position.y}
+	data["ball"] = ball_data
+	
+	var dimensions_node = scene.get_node("Dimensions")
+	var dimensions_data = []
+	for tilemap_id in dimensions_node.dimensions.size():
+		var tilemap = dimensions_node.dimensions[tilemap_id]
+		var layers = {}
+		for layer_id in tilemap.get_layers_count():
+			var layer_name = tilemap.get_layer_name(layer_id)
+			var used_cells = tilemap.get_used_cells(layer_id)
+			var layer_tile_data = {}
+			for pos in used_cells:
+				var source_id = tilemap.get_cell_source_id(layer_id, pos)
+				var atlas_cords = tilemap.get_cell_atlas_coords(layer_id, pos)
+				if pos.x not in layer_tile_data:
+					layer_tile_data[pos.x] = {}
+				layer_tile_data[pos.x][pos.y] = [source_id, atlas_cords.x, atlas_cords.y]
+			layers[layer_name] = layer_tile_data
+		dimensions_data.append({
+			"ground": layers["ground"],
+			 "walls": layers["walls"],
+			 "goal": layers["goal"]})
+	data["dimensions"] = dimensions_data
+	
+	var file = FileAccess.open(path, FileAccess.WRITE)
+	file.store_line(JSON.stringify(data))
